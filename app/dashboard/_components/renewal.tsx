b568ec47e96dcb3beabc83cfe85b085080e78716
klaus-gudy <klausongudy@gmail.com>
2024-04-27 13:20:05 +0300

    🚧chore: created a card components to display tenants that need to review their lease agreement

---
+++ app/dashboard/_components/renewal.tsx
@@ -0,0 +1,54 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+
+export function Renewal() {
+    return (
+      <div className="space-y-8">
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/01.png" alt="Avatar" />
+            <AvatarFallback>OM</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Olivia Martin</p>
+            <p className="text-sm text-muted-foreground">0674****80<span className="cursor-pointer hover:text-black">.Remind</span></p>
+          </div>
+          <div className="ml-auto font-medium text-sm">14 days left</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="flex h-9 w-9 items-center justify-center space-y-0 border">
+            <AvatarImage src="/avatars/02.png" alt="Avatar" />
+            <AvatarFallback>JL</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Jackson Lee</p>
+            <p className="text-sm text-muted-foreground">0783****80<span className="cursor-pointer hover">.Remind</span></p>
+          </div>
+          <div className="ml-auto font-medium text-sm">21 days left</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/03.png" alt="Avatar" />
+            <AvatarFallback>IN</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
+            <p className="text-sm text-muted-foreground">
+              0672****63
+            </p>
+          </div>
+          <div className="ml-auto font-medium text-sm">30 days left</div>
+        </div>
+        <div className="flex items-center">
+          <Avatar className="h-9 w-9">
+            <AvatarImage src="/avatars/04.png" alt="Avatar" />
+            <AvatarFallback>WK</AvatarFallback>
+          </Avatar>
+          <div className="ml-4 space-y-1">
+            <p className="text-sm font-medium leading-none">William Kim</p>
+            <p className="text-sm text-muted-foreground">0754****89</p>
+          </div>
+          <div className="ml-auto font-medium text-sm">60 days left</div>
+        </div>
+      </div>
+    )
+  }
