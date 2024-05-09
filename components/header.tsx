3f858874d28fc1c56994e2fe5b461e96978254a8
klaus-gudy <klausongudy@gmail.com>
2024-05-09 09:08:27 +0300

    Added new components to be accomodated in the new dashboard layout

---
+++ components/header.tsx
@@ -0,0 +1,30 @@
+"use client"
+import { useSideBarToggle } from "@/hooks/use-sidebar-toggle";
+import { BsList } from "react-icons/bs";
+import classNames from "classnames";
+import UserProfile from "./common/user-profile";
+
+export default function Header() {
+
+    const { toggleCollapse, invokeToggleCollapse } = useSideBarToggle();
+    const sidebarToggle = () => {
+        invokeToggleCollapse();
+    }
+    const headerStyle = classNames("fixed w-full z-[99999] px-4 shadow-sm shadow-slate-500/10",
+        {
+            ["sm:pl-[20rem]"]: !toggleCollapse,
+            ["sm:pl-[5.6rem]"]: toggleCollapse,
+        });
+    return (
+      <header className={headerStyle}>
+        <div className="flex items-center justify-between h-16">
+          <button onClick={sidebarToggle} className="order-2 sm:order-1 bg-rchsDark text-[#ffffff] hover:bg-rchsLight ml-3 rounded-md h-[30px] w-[30px] shadow-md shadow-black/10 transition duration-300 ease-in-out flex items-center justify-center">
+            <BsList></BsList>
+          </button>
+          <div className="order-1 sm:order-2 h-10 w-10 rounded-full bg-rchs flex items-center justify-center text-center">
+          <UserProfile />
+          </div>
+        </div>
+      </header>
+    );
+  }
---
+++ components/sidebar.tsx
@@ -0,0 +1,46 @@
+"use client"
+import Image from "next/image";
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+import { useSideBarToggle } from "@/hooks/use-sidebar-toggle";
+import SideBarMenuGroup from "./sidebar-menu-group";
+import { SIDENAV_ITEMS } from "@/components/menu_constants";
+
+export const SideBar = () => {
+  const [mounted, setMounted] = useState(false);
+  const { toggleCollapse } = useSideBarToggle();
+
+  const asideStyle = classNames(
+    "sidebar overflow-y-auto overflow-x-auto fixed bg-rchs h-full shadow-sm shadow-slate-500/40 transition duration-300 ease-in-out z-[99999]",
+    {
+      ["w-[20rem]"]: !toggleCollapse,
+      ["sm:w-[5.4rem] sm:left-0 left-[-100%]"]: toggleCollapse,
+    }
+  );
+
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <aside className={asideStyle}>
+      <div className="flex relative items-center py-5 px-3.5">
+        <Image
+          alt="RCHS System"
+          src="/logo.png"
+          className="w-12 mx-3.5 min-h-fit"
+          width={35}
+          height={35}
+        ></Image>
+        {!toggleCollapse && (
+          <h3 className="pl-2 font-bold text-2xl">RCHS System</h3>
+        )}
+      </div>
+      <nav className="flex flex-col gap-2 transition duration-300 ease-in-out">
+        <div className="flex flex-col gap-2 px-4">
+          {SIDENAV_ITEMS.map((item, idx) => {
+            return <SideBarMenuGroup key={idx} menuGroup={item} />;
+          })}
+        </div>
+      </nav>
+    </aside>
+  );
+};
